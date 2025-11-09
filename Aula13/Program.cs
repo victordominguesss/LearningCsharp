03ceda5f168abc2c442cd98637cc9d0844ece3d8
victordominguesss <victordominguesince01@gmail.com>
2025-11-09 19:15:13 -0300

    Lesson 13: Do while loop example

---
+++ Aula13/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace  Aula13;
+
+public class Program
+{
+    public static void Main()
+    {
+        int iterable = 0;
+
+        do
+        {
+            Console.WriteLine(iterable);
+            iterable++;
+        }
+        while (iterable <= 10);
+    }
+}
