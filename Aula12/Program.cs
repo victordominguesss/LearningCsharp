9cffe725f4fd069995625bc69b819ed06fe8613d
victordominguesss <victordominguesince01@gmail.com>
2025-11-09 18:17:51 -0300

    Lesson 12: While loop examples (increment and decrement)

---
+++ Aula12/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("========= MENU WHILE =========");
+        Console.WriteLine("Seleciona um dos exemplos abaixo: ");
+        Console.WriteLine("1. WHILE adição de iterações");
+        Console.WriteLine("2. WHILE subtração de iterações");
+        int UserResponse = Convert.ToInt32(Console.ReadLine());
+
+        
+
+        switch (UserResponse)
+        {
+            case 1:
+                int iterable = 0;
+                while (iterable <= 5)
+                {
+                    Console.WriteLine(iterable);
+                    iterable++;
+                }
+                break;
+
+            case 2:
+                double budget = 100;
+                while(budget >= 0)
+                {
+                    Console.WriteLine(budget);
+                    budget--;
+                }
+                Console.WriteLine("Saldo insuficiente!");
+                break;
+        }
+    }
+}
