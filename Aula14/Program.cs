0bb43f2e8a38e6bb314ab268ccaf8394633a5577
victordominguesss <victordominguesince01@gmail.com>
2025-11-16 10:39:42 -0300

    Lesson 14: For loop with multiplication table

---
+++ Aula14/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula14;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===========TABELA DE MULTIPLICAÇÃO===========");
+        for (int i = 1; i <= 10; i++)
+        {
+            for(int j = 1; j <= 10; j++)
+            {
+                Console.Write($"{i * j, 5}");
+            }
+            Console.WriteLine();
+        }
+
+    }   
+}
