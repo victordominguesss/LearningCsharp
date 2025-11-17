3e584d76320f977e7f356aa1a4ac6b8d7394e2c0
victordominguesss <victordominguesince01@gmail.com>
2025-11-17 17:23:38 -0300

    Lesson 18: Creating files with System.IO

---
+++ Aula18/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Aula18;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        string path = @"C:\Users\victor.domingues\LearningCsharp\Aula18\";
+
+        string fileName = "myDocument.Doc";
+
+        string filePath = path + fileName;
+
+        string content = "Hello world!";
+
+        //Criar arquivo doc com conteúdo
+        File.WriteAllText(filePath, content);
+
+        Console.WriteLine("Arquivo criado com sucesso!");
+
+        string additiolContent = "| em Csharp";
+
+        //Adicionar conteúdo a um arquivo já existente
+        File.AppendAllText(filePath, additiolContent);
+
+        Console.WriteLine("Conteúdo adicionado com sucesso!");
+
+
+        string fileContent = File.ReadAllText(filePath);
+
+        Console.WriteLine("Conteúdo do arquivo: " + fileContent);
+    }
+}
