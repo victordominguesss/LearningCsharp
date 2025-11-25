5f359cb0d615c03cbbecf18ad0d1bb4723f789fc
victordominguesss <victordominguesince01@gmail.com>
2025-11-25 08:52:49 -0300

    Instantiating a class and calling a method with no return value.

---
+++ Aula21/Bank.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula21;
+
+public class Bank
+{
+    //Atributos
+    public int Id;
+    public string Name;
+
+    //Métodos (sem retorno)
+    public void RequestNewCard()
+    {
+        Console.WriteLine("Enviando email...");
+        Console.WriteLine("Solicite um novo cartão");
+    }
+}
---
+++ Aula21/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula21;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Criando a instância 
+        Bank Itau = new Bank();
+
+        //Chamando um método da instância da classe bank 
+        Itau.RequestNewCard();
+    }
+}
