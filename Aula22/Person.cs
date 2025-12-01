0fe1e523ec2f5e8e69b815fbd836ef6ec75d31d4
victordominguesss <victordominguesince01@gmail.com>
2025-12-01 08:56:28 -0300

    Lesson 22: Method with return

---
+++ Aula22/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula22;
+
+public class Person
+{
+    public string Name;
+    public int Age;
+
+    public string IfLegalPerson(int age, string name)
+    {
+        string response = string.Empty;
+
+        if (age >= 18)
+        {
+            response = $"Confirmado, {name} é maior de idade";
+            return response;
+        }
+        else
+        {
+            response = $"{name} não é maior de idade";
+            return response;
+        }
+    }
+}
---
+++ Aula22/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula22;
+
+public class Program
+{
+   public static void Main()
+    {
+        Person person = new Person();
+
+        Console.WriteLine("Digite seu nome");
+        string name = Console.ReadLine(); 
+
+        Console.WriteLine("Digite sua idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        string response = person.IfLegalPerson(age, name);
+        Console.WriteLine(response);
+    }
+}
