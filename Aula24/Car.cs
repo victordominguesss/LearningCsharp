279e196df56bad0308a09e126240cb2be82d1b45
victordominguesss <victordominguesince01@gmail.com>
2025-12-02 09:00:40 -0300

    Lesson 24: Constructors

---
+++ Aula24/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula24;
+
+public class Car(int id, string brand, string model, int year, string color)
+{
+    public int Id = id;
+    public string Brand = brand;
+    public string Model = model;
+    public int Year = year;
+    public string Color = color;
+}
---
+++ Aula24/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula24;
+
+public class Program
+{
+    public static void Main()
+    {
+        Car HondaCivic = new(1, "Honda", "Touring", 2020, "Silver");
+
+        Car FiatUno = new(2, "Fiat", "UnoFire", 2021, "Black");
+
+        var carList = new List<Car> { HondaCivic, FiatUno };
+
+        foreach(Car car in carList)
+        {
+            Console.WriteLine("========= Cars =========");
+            Console.WriteLine($"Id: {car.Id}");
+            Console.WriteLine($"Marca: {car.Brand}");
+            Console.WriteLine($"Modelo: {car.Model}");
+            Console.WriteLine($"Ano: {car.Year}");
+            Console.WriteLine($"Cor: {car.Color}");
+        }
+    }
+}
